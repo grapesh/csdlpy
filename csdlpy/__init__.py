a678cc5c32ae30a4d92fade4c718f67f708ded22
J.R. Calzada <jreniel@gmail.com>
2019-06-14 13:14:17 -0400

    Added setup.py

--- csdlpy/__init__.py
+++ csdlpy/__init__.py
@@ -7,7 +7,6 @@ import interp
 import valstat
 import transfer
 import etss
-from .version import __version__
-__all__ = ['adcirc', 'estofs', 'obs', 'atcf', 'plotter', 'interp', 'valstat', 'transfer','etss']
 
-
+__all__ = ['adcirc', 'estofs', 'obs', 'atcf', 'plotter', 'interp', 'valstat',
+           'transfer', 'etss']
